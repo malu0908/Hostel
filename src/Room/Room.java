020cb5a286c17535db67a1fc229b230139ccb788
Denis Coutinho <56079372+DenisCoutinho@users.noreply.github.com>
2019-10-12 15:16:50 -0300

    Room e DailyRate
    Cabeçalho e Getters and Setters das classes Room e DailyRate

---
+++ src/Room/Room.java
@@ -0,0 +1,22 @@
+public class Room {
+	
+	private int number;
+	private double dimension;
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public double getDimension() {
+		return dimension;
+	}
+
+	public void setDimension(double dimension) {
+		this.dimension = dimension;
+	}
+
+}
