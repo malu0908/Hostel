704cd7f4ecc42b683f23c4585335d26538886192
Luana de Jesus Carvalho <luanadejcarvalho@gmail.com>
2019-10-18 00:00:20 -0300

    Criando os atributos e construtores da classe Customer

--- src/customer/Customer.java
+++ src/customer/Customer.java
@@ -1,5 +1,33 @@
 package customer;
 
-public class Customer {
+import java.time.LocalDate;
 
+public class Customer {
+	private String title;
+	private String firstname;
+	private String lastname;
+	private LocalDate birthday;
+	private String email;
+	
+	public Customer(String title) {
+		this.title = title;
+	}
+	
+	public Customer(String title, String firstname) {
+		this.title = title;
+		this.firstname = firstname;
+	}
+	
+	public Customer(String title, String firstname, String lastname) {
+		this.title = title;
+		this.firstname = firstname;
+		this.lastname = lastname;
+	}
+	
+	public Customer(String title, String firstname, String lastname, LocalDate birthday) {
+		this.title = title;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.birthday = birthday;
+	}
 }
